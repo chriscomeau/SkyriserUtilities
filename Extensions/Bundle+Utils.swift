1e25d766ae608c7be7589cdff7cb4d1614285569
Chris Comeau <chris.comeau@skyriser.com>
2019-01-19 18:11:24 -0500

    Create Bundle+Utils.swift

---
+++ Extensions/Bundle+Utils.swift
@@ -0,0 +1,28 @@
+//
+//  UIColors+Utils.swift
+//
+//  Created by Chris Comeau on 2019-01-19.
+//  Copyright © 2019 Skyriser Media. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension Bundle {
+
+  var appName: String {
+    return infoDictionary?["CFBundleName"] as! String
+  }
+  
+  var bundleId: String {
+    return bundleIdentifier!
+  }
+  
+  var versionNumber: String {
+    return infoDictionary?["CFBundleShortVersionString"] as! String
+  }
+  
+  var buildNumber: String {
+    return infoDictionary?["CFBundleVersion"] as! String
+  }
+}
